656d4c4626d7b2bbcca5fe7e2b318af1fa28034e
DavidPH1301 <dphidalgo@gmail.com>
2026-08-16 16:36:50 +0200

    Add files via upload

--- js/tmtcicd.js
+++ js/tmtcicd.js
@@ -40,7 +40,6 @@ function updateArticle(type, stype)
     if ((pktType == type)&&(pktSubtype == stype))
     {  
       document.getElementById("packet-name").innerHTML = '<strong>NAME: </strong>' + pktList[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-      document.getElementById("packet-apid").innerHTML = '<strong>APIDs: </strong>' + pktList[i].getElementsByTagName("apids")[0].childNodes[0].nodeValue;
       document.getElementById("packet-desc").innerHTML = '<strong>DESCRIPTION: </strong>' + pktList[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
       
       // TODO: here check the number of parameters and add a row for each. This is the case of no parameters (none)
